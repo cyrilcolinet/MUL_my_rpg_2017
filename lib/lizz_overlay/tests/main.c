e8a57731d22fa6c12c534ee3e46d0f1aee8611d5
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-21 23:03:50 +0100

    :dart: Fix warning

--- lib/lizz_overlay/tests/main.c
+++ lib/lizz_overlay/tests/main.c
@@ -12,12 +12,17 @@ int main(void)
 	sfVideoMode video = { 1600, 800, 32 };
 	sfRenderWindow *win = NULL;
 	sfEvent event;
+	btn_t *btn = NULL;
 
 	if (lizz_start(true) < 0) // Start lizz overlay
 		return (84);
 
 	win = sfRenderWindow_create(video, "Lizz Overlay", sfClose, NULL);
 	sfRenderWindow_setFramerateLimit(win, 60);
+	lizz_btn_create("fraise", stMain);
+
+	btn = lizz_get_btn("fraise", stMain);
+	btn->setTexture(btn, "a.png", NULL);
 
 	while (sfRenderWindow_isOpen(win)) {
 		while (sfRenderWindow_pollEvent(win, &event))
