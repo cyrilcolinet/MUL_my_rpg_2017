896ce6371ac2a42a5ec7ce31e502be87b2042d77
Cyril COLINET <cyril.colinet@epitech.eu>
2018-04-24 16:06:32 +0200

    :tada: Add configuraton for buttons and background manager

--- src/configuration/menus/main_menu_config.c
+++ src/configuration/menus/main_menu_config.c
@@ -7,6 +7,14 @@
 
 # include "rpg.h"
 
+static void configure_buttons(rpg_t *rpg)
+{
+	btn_t *btn = NULL;
+
+	btn = lizz_get_btn(rpg->lizz, "btn_play", stMain);
+	btn->setTexture(btn, "assets/buttons/buttons.png", NULL);
+}
+
 void main_menu_config(rpg_t *rpg)
 {
 	lizz_btn_create(rpg->lizz, "btn_play", stMain);
@@ -14,4 +22,5 @@ void main_menu_config(rpg_t *rpg)
 	lizz_btn_create(rpg->lizz, "btn_credits", stMain);
 	lizz_btn_create(rpg->lizz, "btn_quit", stMain);
 	lizz_thread_create(rpg->lizz, "th_bg", &bg_task, &rpg);
+	configure_buttons(rpg);
 }
