e2743b1058c7f9d92acb9f0b1c7276395e1542d5
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-13 23:19:24 +0200

    Add structs

--- src/parser/map_parser.c
+++ src/parser/map_parser.c
@@ -7,42 +7,22 @@
 
 # include "rpg.h"
 
-int parse_map_hitboxes(char *file, rpg_t *rpg)
-{
-	int fd = -1;
-	char *line = NULL;
-
-	if (file == NULL)
-		return (-1);
-
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (-1);
-
-	while ((line = get_next_line(fd)) != NULL) {
-		printf("%s\n", line);
-		free(line);
-	}
-
-	return (0);
-}
-
-int parse_arena_rectangle_values(config_setting_t *arena, interact_t **it)
+int parse_arena_rectangle_values(config_setting_t *arena, map_t **map)
 {
 	config_setting_t *rec = config_setting_lookup(arena, "rec");
 
 	if (rec == NULL)
 		return (-1);
 
-	config_setting_lookup_int(rec, "top", &(*it)->rec.top);
-	config_setting_lookup_int(rec, "left", &(*it)->rec.left);
-	config_setting_lookup_int(rec, "width", &(*it)->rec.width);
-	config_setting_lookup_int(rec, "height", &(*it)->rec.height);
+	config_setting_lookup_int(rec, "top", &(*map)->it->rec.top);
+	config_setting_lookup_int(rec, "left", &(*map)->it->rec.left);
+	config_setting_lookup_int(rec, "width", &(*map)->it->rec.width);
+	config_setting_lookup_int(rec, "height", &(*map)->it->rec.height);
 
 	return (0);
 }
 
-int parse_arenas_values(rpg_t *rpg, interact_t **it, config_setting_t *set)
+int parse_arenas_values(map_t **map, config_setting_t *set)
 {
 	config_setting_t *arenas = config_setting_lookup(set, "arenas");
 	config_setting_t *arena = NULL;
@@ -57,8 +37,8 @@ int parse_arenas_values(rpg_t *rpg, interact_t **it, config_setting_t *set)
 		if (arena == NULL)
 			return (-1);
 		config_setting_lookup_string(arena, "message", \
-		((const char **)&(*it)->msg));
-		if (parse_arena_rectangle_values(arena, it) != 0)
+		((const char **)&(*map)->it->msg));
+		if (parse_arena_rectangle_values(arena, map) != 0)
 			return (-1);
 	}
 
@@ -74,16 +54,16 @@ int parse_map_values(rpg_t *rpg, config_setting_t *set, int key)
 	rpg->map[key] = malloc(sizeof(map_t));
 	if (rpg->map[key] == NULL)
 		return (-1);
-	rpg->map[key]->interaction = malloc(sizeof(interact_t));
-	if (rpg->map[key]->interaction == NULL)
+	rpg->map[key]->it = malloc(sizeof(interact_t));
+	if (rpg->map[key]->it == NULL)
 		return (-1);
 
 	config_setting_lookup_string(set, "hitboxes", ((const char **)&hit));
 	config_setting_lookup_string(set, "map", \
 	((const char **)&rpg->map[key]->texture));
-	if (parse_map_hitboxes(hit, rpg) != 0)
+	if (parse_map_hitboxes(hit, rpg->map[key]) != 0)
 		return (-1);
-	if (parse_arenas_values(rpg, &rpg->map[key]->interaction, set) != 0)
+	if (parse_arenas_values(&rpg->map[key], set) != 0)
 		return (-1);
 
 	return (0);
