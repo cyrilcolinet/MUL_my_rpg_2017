4b1cbbcba7cbfe1b78cc79f094adb15294d20dad
Marcel Lucas <lucas.marcel@epitech.eu>
2018-05-13 12:37:25 +0200

    display attack value spell

--- src/battle/spell/cast_heal.c
+++ src/battle/spell/cast_heal.c
@@ -26,6 +26,32 @@ static void display_heal(rpg_t *rpg, battle_t *battle, int i, int heal)
 	sfText_setColor(battle->text, sfWhite);
 }
 
+static void heal_the_target(battle_t *battle, int i, int val)
+{
+	int x = i % 12;
+	int y = i / 12;
+	int a = 0;
+	int b = 0;
+
+	if (battle->map[y][x] == 1) {
+		if (battle->hero->hp + val >= battle->hero->hp_max)
+			battle->hero->hp = battle->hero->hp_max;
+		else
+			battle->hero->hp += val;
+	}
+	for (int j = 0 ;j < battle->fight[
+	battle->id]->number_enemy; j++) {
+		a = battle->fight[battle->id]->enemy[j]->pos.x;
+		b = battle->fight[battle->id]->enemy[j]->pos.y;
+		if (battle->map[y][x] == 2 && x == a && y == b
+		&& battle->hero->hp + val >= battle->hero->hp_max)
+			battle->fight[battle->id]->enemy[j]->hp =
+			battle->fight[battle->id]->enemy[j]->hp_max;
+		else if (battle->map[y][x] == 2 && x == a && y == b)
+			battle->fight[battle->id]->enemy[j]->hp += val;
+	}
+}
+
 static void draw_anim(rpg_t *rpg, battle_t *battle, int i)
 {
 	sfVector2f pos = battle->hero->spell[0]->pos;
@@ -51,6 +77,8 @@ static void draw_anim(rpg_t *rpg, battle_t *battle, int i)
 
 static void reset_spell(battle_t *battle)
 {
+	sfVector2f pos = battle->hero->spell[1]->pos;
+
 	battle->hero->rec.top -= 4 * 64;
 	battle->hero->rec.left = 0;
 	sfSprite_setTextureRect(battle->hero->form,
@@ -60,6 +88,8 @@ static void reset_spell(battle_t *battle)
 	sfSprite_setTextureRect(battle->hero->spell[0]->form,
 			battle->hero->spell[0]->rec);
 	battle->hero->attack = true;
+	heal_the_target(battle, pos.y * 12 + pos.x,
+			battle->hero->spell[0]->val);
 }
 
 void cast_heal(rpg_t *rpg, battle_t *battle)
@@ -69,6 +99,7 @@ void cast_heal(rpg_t *rpg, battle_t *battle)
 	pos.x = battle->hero->spell[0]->pos.x * B_X + MAP_X - 8;
 	pos.y = battle->hero->spell[0]->pos.y * B_Y + MAP_Y - 32;
 	sfSprite_setPosition(battle->hero->spell[0]->form, pos);
+	reset_map_state(battle);
 	for (int i = 0; i < 18;) {
 		sfClock_restart(battle->clock);
 		battle->time = sfTime_Zero;
