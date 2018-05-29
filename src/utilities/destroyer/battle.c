899a740a7f0880000bd7e970c7b1748cc46ca6b7
Cyril COLINET <cyril.colinet@epitech.eu>
2018-05-29 18:23:32 +0200

    :tea: Restruct code

--- src/utilities/destroyer/battle.c
+++ src/utilities/destroyer/battle.c
@@ -7,16 +7,47 @@
 
 # include "rpg.h"
 
-void destroy_content(rpg_t *rpg)
+void free_layer(int **layer)
 {
-	for (int i = 0; rpg->map[i]; i++) {
-		sfSprite_destroy(rpg->map[i]->sprite);
-		for (int j = 0; rpg->map[i]->h_layer[j]; j++)
-			free(rpg->map[i]->h_layer[j]);
-		free(rpg->map[i]->h_layer);
-		free(rpg->map[i]->it);
-		free(rpg->map[i]);
+	for (int i = 0; i < 34; i++)
+		free(layer[i]);
+	free(layer);
+}
+
+void free_fight(battle_t *battle, int n)
+{
+	for (int i = 0; i < 120; i++)
+		sfRectangleShape_destroy(battle->fight[n]->map[i]);
+
+	free(battle->fight[n]->map);
+
+	for (int i = 0; i < battle->fight[n]->number_enemy; i++) {
+		sfSprite_destroy(battle->fight[n]->enemy[i]->form);
+		sfRectangleShape_destroy(battle->fight[n]->enemy[i]->frame);
+		free(battle->fight[n]->enemy[i]);
 	}
-	
-	free(rpg->map);
+
+	free(battle->fight[n]->enemy);
+	free(battle->fight[n]);
+}
+
+void free_battle(rpg_t *rpg)
+{
+	for (int i = 0; i < rpg->battle->number_fight; i++)
+		free_fight(rpg->battle, i);
+
+	free(rpg->battle->fight);
+	sfSprite_destroy(rpg->battle->hero->form);
+	sfText_destroy(rpg->battle->text);
+	sfClock_destroy(rpg->battle->clock);
+	free(rpg->battle->hero);
+	for (int i = 0; i < 10; i++)
+		free(rpg->battle->map[i]);
+	free(rpg->battle->map);
+	sfRectangleShape_destroy(rpg->battle->icone);
+	sfRectangleShape_destroy(rpg->battle->background);
+	free(rpg->battle->texture);
+	free(rpg->player->render_state);
+	free_layer(rpg->player->layer);
+	free(rpg->player);
 }
