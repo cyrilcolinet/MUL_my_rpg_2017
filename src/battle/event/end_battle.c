df443a3d60044441ce538805094bf444b0443801
Marcel Lucas <lucas.marcel@epitech.eu>
2018-06-01 09:01:36 +0200

    end battle .c modif

---
+++ src/battle/event/end_battle.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2018
+** end_battle
+** File description:
+** rpg
+*/
+
+#include "rpg.h"
+
+static void end_battle_display(rpg_t *rpg, battle_t *battle)
+{
+	for (int i = 0; i < 4; i++)
+		battle->hero->spell[i]->cast = false;
+	battle->hero->alive = true;
+	battle->hero->hp = battle->hero->hp_max;
+	battle->hero->select = false;
+	battle->hero->played = false;
+	battle->hero->move = false;
+	battle->hero->attack = false;
+	battle->fight[battle->id]->enemy_turn = false;
+	battle->run = false;
+}
+
+bool end_battle(rpg_t *rpg, battle_t *battle)
+{
+	if (!battle->hero->alive) {
+		printf("YOU LOSE\n");
+		end_battle_display(rpg, battle);
+		return (true);
+	}
+	for (int i = 0; i < battle->fight[battle->id]->number_enemy; i++) {
+		if (battle->fight[battle->id]->enemy[i]->alive)
+			return (false);
+	}
+	printf("YOU WIN\n");
+	end_battle_display(rpg, battle);
+	return (true);
+}
