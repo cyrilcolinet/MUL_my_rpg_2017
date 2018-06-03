fd13c9a3ef9890de461b27f6e4c159bda11b8abe
Cyril COLINET <cyril.colinet@epitech.eu>
2018-06-03 23:30:05 +0200

    :dart: Fix return button

--- src/player/go_to_spawn.c
+++ src/player/go_to_spawn.c
@@ -11,11 +11,5 @@ void go_to_spawn(rpg_t *rpg, int id)
 {
 	rpg->map[rpg->map_id]->it[id]->active = false;
 	rpg->map[rpg->map_id]->it[id]->in_zone = false;
-	/* free_battle(rpg); */
-	/* sfSprite_destroy(rpg->player->sprite); */
-	/* configure_internal_struct(rpg); */
-	/* rpg->state = gameWait; */
-	/* rpg->last_st = gameUnknown; */
-	/* reset_to_normal_rect(rpg); */
 	cb_goto_main_view(rpg, NULL);
 }
